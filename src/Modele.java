18c2a30bbd6adeaab02ac7958e498e344da7285e
Gregoire Casedevant <gcasedev@pcalb-mm0000.univ-jfc.fr>
2019-02-13 11:06:04 +0100

    ajout des 2 classe modele et range (imcomplète)

---
+++ src/Modele.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+import java.util.Enumeration;
+
+public class Modele {
+	
+	private static Color[] couleur = {Color.yellow,Color.green,Color.blue,Color.magenta,Color.red,Color.orange,Color.white,Color.black};
+	int n_tentative;
+	int difficulte;
+	Enumeration etat;
+
+}
