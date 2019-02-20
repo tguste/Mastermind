ea183e7cb16a06dd655b1d5df0ad7e292e39efbe
Thibault Deuve <tdeuve@pcalb-mm0000.univ-jfc.fr>
2019-02-20 11:54:10 +0100

    v0.lol

--- src/VueClavier.java
+++ src/VueClavier.java
@@ -1,6 +1,7 @@
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.List;
 import java.awt.Panel;
 import java.awt.event.ActionListener;
@@ -14,11 +15,12 @@ public class VueClavier extends Panel{
 	public VueClavier(ActionListener a) {
 		super();
 		this.setBackground(Color.white);
-		Button b = new Button();
+		
 		ArrayList<Button> liste_bouttons= new ArrayList<Button>();
 		for (int i = 0; i<Modele.COULEUR.length;i++) {
+			Button b = new Button(" ");
 			b.setBackground(Modele.COULEUR[i]);
-			b.setSize(200,200);
+			b.setPreferredSize(new Dimension(20,20));
 			liste_bouttons.add(b);
 			liste_bouttons.get(i).addActionListener(a);
 			this.add(liste_bouttons.get(i));
--- src/VuePropositions.java
+++ src/VuePropositions.java
@@ -6,16 +6,55 @@ import java.awt.event.WindowListener;
 
 public class VuePropositions extends Canvas  {
 
+	public static final int JX_Y=10;
+	public static final int ECART=60;
+	
+	public static final int CX=250;
+	public static final int CY=8;
+	
+	public final static int RJ=45;
+	public final static int RC=15;
+	
 	public VuePropositions() {
 		super();
-		this.setSize(Modele.N_TENTATIVE*150,Modele.DIFFICULTE*150);
+		this.setSize(Modele.DIFFICULTE*75,Modele.N_TENTATIVE*70);
 		this.setBackground(Color.GRAY);
 		
 		
 	}
 	public void paint(Graphics g) {
+		int a=0;
+		for (int i =0; i<10;i++) {
+			g.drawOval(JX_Y,(JX_Y*i)+a,RJ, RJ);
+			a+=ECART;
+			}
 		
 		
-	}
+		
+		/*
+		g.drawOval(10, 10, 45, 45);
+		g.drawOval(10, 70, 45, 45);
+		g.drawOval(10, 130, 45, 45);
+		g.drawOval(10, 190, 45, 45);
+		g.drawOval(10, 250, 45, 45);
+		g.drawOval(10, 310, 45, 45);
+		g.drawOval(10, 370, 45, 45);
+		g.drawOval(10, 430, 45, 45);
+		g.drawOval(10, 490, 45, 45);
+		g.drawOval(10, 550, 45, 45);
+		
+	
+		g.drawOval(70, 10, 45, 45);
+		g.drawOval(130, 10, 45, 45);
+		g.drawOval(190, 10, 45, 45);
+		
+		g.drawOval(250, 8, 15, 15);
+		g.drawOval(250, 30, 15, 15);
+		g.drawOval(275, 8, 15, 15);
+		g.drawOval(275, 30, 15, 15);
+		*/
+		
+	
+	
 
 }
